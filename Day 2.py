515530afc07b4faf45659fe7a6db3540ebaff21d
mythri07 <72002070+mythri07@users.noreply.github.com>
2021-04-22 12:36:13 -0400

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,10 @@
+Car_name="Volvo"
+Car_color="Black"
+Car_price=390000.59
+Car_speed=100
+isSafe=True
+print("My Car name is {} \n
+Color of the car is {}\n
+Price of the car is {}\n
+Speed of the car is {}\n
+{} car is safe to drive".format(Car_name,Car_color,Car_price,Car_speed,Car_name))
